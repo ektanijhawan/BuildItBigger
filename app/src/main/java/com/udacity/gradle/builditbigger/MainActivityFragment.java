5ce2e046b58d87cddaeec5b831dde390abfd546d
Ekta Nijhawan <ektanijhawan1@gmail.com>
2016-11-06 23:56:53 +0530

    Java Lib-Added

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -8,8 +8,7 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-
-
+import com.example.Joker;
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -22,7 +21,7 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-
+  Joker joker= new Joker();
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
