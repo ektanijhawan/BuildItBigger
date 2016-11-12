8f7600bd97ace117315c1ab77014c56307267c3f
Ekta Nijhawan <ektanijhawan1@gmail.com>
2016-11-13 02:27:58 +0530

    TestCase-Added

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/TestClass.java
@@ -0,0 +1,45 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.annotation.UiThreadTest;
+import android.test.AndroidTestCase;
+
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
+
+/**
+ * Created by Ekta on 13-11-2016.
+ */
+
+public class TestClass  implements EndpointsAsyncTask.EndpoinrResponseInterface{
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    private final CountDownLatch mSignal = new CountDownLatch(1);
+
+    @Test
+    public void testJokeRetriever() {
+        new EndpointsAsyncTask(this ).execute();
+        try {
+            boolean success = mSignal.await(5, TimeUnit.SECONDS);
+            if (!success) {
+                fail("Test timed out, make sure the server is actually running.");
+            }
+        } catch (InterruptedException e) {
+            fail();
+        }
+    }
+
+    @Override
+    public void onResponse(boolean isSuccess, String result) {
+        assertTrue(isSuccess && result != null && result.length() > 0);
+        mSignal.countDown();
+    }
+}
