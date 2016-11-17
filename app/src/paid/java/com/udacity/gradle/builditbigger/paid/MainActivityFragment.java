3aa54a5519aa36f42f43aaf324fa4fb42f0aeeaa
Ekta Nijhawan <ektanijhawan1@gmail.com>
2016-11-18 01:20:04 +0530

    Paid version-works

--- app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
@@ -1,7 +1,6 @@
-package com.udacity.gradle.builditbigger.free;
+package com.udacity.gradle.builditbigger.paid;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +13,9 @@ import com.google.android.gms.ads.AdView;
 import com.udacity.gradle.builditbigger.R;
 
 
+/**
+ * A simple {@link Fragment} subclass.
+ */
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
@@ -23,15 +25,8 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main_activity, container, false);
-       Joker joker= new Joker();
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
+
+
 
         return root;
     }
